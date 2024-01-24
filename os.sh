47ff633497b1ad28f9ed337faf67df76f3a833f4
ruingl <149220283+ruingl@users.noreply.github.com>
2024-01-24 09:47:11 +0000

    it finally booted to grub !1!1!

--- os.sh
+++ os.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Ensure that the build directory exists
+mkdir -p build
+
 # Compile and Link
-nasm -f elf32 Boot.asm
-ld -T link.ld -melf_i386 Boot.o -o iso/boot/kernel
+nasm -f elf32 Boot.asm -o build/boot.o
+gcc -m32 -c src/main.c -o build/main.o
+ld -T link.ld -melf_i386 build/boot.o build/main.o -o iso/boot/kernel
 
 # Create ISO
 genisoimage -b boot/grub/stage2_eltorito -R -boot-load-size 4 -no-emul-boot -boot-info-table -A Replit iso > os.iso
 
+# Run in Bochs
 bochs -q
